bcd83d52b0d4641f58abaefa969b41393eb792ed
Ransom <ransom@venveo.com>
2020-11-01 13:34:37 -0500

    Added associations to sync messages

--- src/entities/ecommerce/ExternalSyncMessage.php
+++ src/entities/ecommerce/ExternalSyncMessage.php
@@ -17,6 +17,10 @@ class ExternalSyncMessage extends HubSpotEntity
     public const ACTION_UPSERT = 'UPSERT';
     public const ACTION_DELETE = 'DELETE';
 
+    public const ASSOCIATION_DEAL = 'DEAL';
+    public const ASSOCIATION_CONTACT = 'CONTACT';
+    public const ASSOCIATION_PRODUCT = 'PRODUCT';
+
     public string $action = self::ACTION_UPSERT;
     public ?string $changedAt = null;
     public string $externalObjectId = '';
@@ -29,5 +33,18 @@ class ExternalSyncMessage extends HubSpotEntity
         $rules[] = [['action', 'externalObjectId', 'properties'], 'required'];
         $rules[] = ['action', 'in', 'range' => [self::ACTION_UPSERT, self::ACTION_DELETE]];
         return $rules;
+    }
+
+    /**
+     * Adds an associated record
+     * @param $associationType
+     * @param $externalObjectId
+     */
+    public function addAssociation($associationType, $externalObjectId): void
+    {
+        if (!isset($this->associations[$associationType])) {
+            $this->associations[$associationType] = [];
+        }
+        $this->associations[$associationType][] = $externalObjectId;
     }
 }
