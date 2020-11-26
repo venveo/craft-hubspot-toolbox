6f200812210ccd2e2434a0aaf3f5624b2d6998a3
Ransom <ransom@venveo.com>
2020-11-25 20:36:35 -0500

    Added some enums for property types

---
+++ src/enums/HubSpotObjectDataTypes.php
@@ -0,0 +1,11 @@
+<?php
+namespace venveo\hubspottoolbox\enums;
+
+abstract class HubSpotObjectDataTypes
+{
+    public const String = 'string';
+    public const Number = 'number';
+    public const Bool = 'bool';
+    public const Datetime = 'datetime';
+    public const Enumeration = 'enumeration';
+}
---
+++ src/enums/HubSpotObjectFieldTypes.php
@@ -0,0 +1,14 @@
+<?php
+namespace venveo\hubspottoolbox\enums;
+
+abstract class HubSpotObjectFieldTypes
+{
+    public const TextArea = 'textarea';
+    public const Select = 'select';
+    public const Text = 'text';
+    public const Date = 'date';
+    public const File = 'file';
+    public const Number = 'number';
+    public const Radio = 'radio';
+    public const Checkbox = 'checkbox';
+}
